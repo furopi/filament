234776e58a871e02b5267b1946c8606862ab0329
furomin <furomin@gmail.com>
2022-12-02 18:19:36 +0100

    Changes in main template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -535,9 +535,11 @@
         </div>
       </div>
 
-      <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
-        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
-      </div>
+      @guest
+        <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+          Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+        </div>
+      @endguest
     </div>
   </div>
 </div>
